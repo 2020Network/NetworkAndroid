bfd3c9eeb44040d25d564056d75f25cdfe8616b8
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2020-11-30 00:25:39 +0900

    <Create> DataClass Signup

---
+++ app/src/main/java/com/example/studygorup/DTO/Signup.kt
@@ -0,0 +1,11 @@
+package com.example.studygorup.DTO
+
+data class Signup(
+    val name : String,
+    val id : String,
+    val pwd1 : String,
+    val pwd2 : String,
+    val age : String,
+    val gender : String,
+    val field : String
+)
