2a4bcf2b479b09922f4ff955f3ec698cbb94c920
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-11-30 02:04:31 +0900

    [CREATE] SignUp Activity

--- app/src/main/java/com/example/studygorup/SignupActivity.kt
+++ app/src/main/java/com/example/studygorup/SignupActivity.kt
@@ -2,10 +2,34 @@ package com.example.studygorup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val spinner = findViewById(R.id.spinner) as Spinner
+        val adapter = ArrayAdapter.createFromResource(this, R.array.field_array, android.R.layout.simple_spinner_item)
+
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Log.d("TAG", "onItemSelected: $position, ${spinner.getItemAtPosition(position)}")
+            }
+        }
     }
 }
