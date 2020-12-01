8ed5d83fc4795bf272f475e449730969e9227611
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-12-01 16:54:11 +0900

    [UPDATE] File Fragment

--- app/src/main/java/com/example/studygorup/FileFragment.java
+++ app/src/main/java/com/example/studygorup/FileFragment.java
@@ -10,19 +10,36 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
-public class FileFragment extends Fragment {
+public class FileFragment extends Fragment implements View.OnClickListener{
     private RecyclerView recyclerView;
     private FileAdapter adapter;
     private ArrayList<String> list = new ArrayList<>();
 
+    View view;
+
+    TextView uploadBtn, postBtn, cancelBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_file, container, false);
+        view = inflater.inflate(R.layout.fragment_file, container, false);
+
+        uploadBtn = view.findViewById(R.id.fileUpload);
+        postBtn = view.findViewById(R.id.postBtn);
+        cancelBtn = view.findViewById(R.id.cancelBtn);
+
+        uploadBtn.setOnClickListener(this);
+        postBtn.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
 
         recyclerView = view.findViewById(R.id.recycler);
 
@@ -45,5 +62,24 @@ public class FileFragment extends Fragment {
         }
 
         return contacts;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fileUpload:
+                Toast.makeText(getContext(), "파일 등록하기", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.postBtn:
+                SlidingDrawer drawer2 = view.findViewById(R.id.sliding_drawer);
+                drawer2.animateClose();
+                Toast.makeText(getContext(), "등록 되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cancelBtn:
+                SlidingDrawer drawer = view.findViewById(R.id.sliding_drawer);
+                drawer.animateClose();
+                Toast.makeText(getContext(), "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
