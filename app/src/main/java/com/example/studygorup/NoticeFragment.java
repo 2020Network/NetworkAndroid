c593a9d9563c3f66468a9d692d7654e9d261a5c7
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-12-01 16:54:42 +0900

    [UPDATE] Notice Fragment

--- app/src/main/java/com/example/studygorup/NoticeFragment.java
+++ app/src/main/java/com/example/studygorup/NoticeFragment.java
@@ -13,40 +13,31 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SlidingDrawer;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class NoticeFragment extends Fragment {
+public class NoticeFragment extends Fragment implements View.OnClickListener{
 
     private RecyclerView recyclerView;
     private NoticeAdapter adapter;
     private ArrayList<NoticeItemData> list = new ArrayList<>();
 
-    ImageView addBtn;
-    Button btnClose;
+    View view;
+
+    TextView cancelBtn, postBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_notice, container, false);
+        view = inflater.inflate(R.layout.fragment_notice, container, false);
 
-        /*btnClose = view.findViewById(R.id.btnClose);
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SlidingDrawer drawer = view.findViewById(R.id.sliding_drawer);
-                drawer.animateClose();
-            }
-        });*/
+        cancelBtn = view.findViewById(R.id.cancelBtn);
+        postBtn = view.findViewById(R.id.postBtn);
 
-        /*addBtn = view.findViewById(R.id.noticeAddBtn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(container.getContext(), "공지를 추가하자", Toast.LENGTH_SHORT).show();
-            }
-        });*/
+        cancelBtn.setOnClickListener(this);
+        postBtn.setOnClickListener(this);
 
         recyclerView = view.findViewById(R.id.recycler);
 
@@ -61,4 +52,19 @@ public class NoticeFragment extends Fragment {
        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.cancelBtn:
+                SlidingDrawer drawer = view.findViewById(R.id.sliding_drawer);
+                drawer.animateClose();
+                Toast.makeText(getContext(), "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.postBtn:
+                SlidingDrawer drawer2 = view.findViewById(R.id.sliding_drawer);
+                drawer2.animateClose();
+                Toast.makeText(getContext(), "등록 되었습니다.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
