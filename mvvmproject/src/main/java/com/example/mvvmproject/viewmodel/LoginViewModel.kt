4df09cd0785c993f88ad050e860fd208377e6a5d
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2021-01-20 23:30:00 +0900

    <Create> MVVM - viewmodel

---
+++ mvvmproject/src/main/java/com/example/mvvmproject/viewmodel/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.mvvmproject.viewmodel
+
+class LoginViewModel {
+
+}
---
+++ mvvmproject/src/main/java/com/example/mvvmproject/viewmodel/SplashViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.mvvmproject.viewmodel
+
+import android.content.Context
+import com.example.mvvmproject.base.BaseViewModel
+import com.example.mvvmproject.widget.SingleLiveEvent
+
+class SplashViewModel() : BaseViewModel() {
+
+}
