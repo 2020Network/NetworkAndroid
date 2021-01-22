b5b0b475b775c9151f6b463319caae8721b720eb
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2021-01-22 23:36:59 +0900

    <Update> LoginViewModel

--- mvvmproject/src/main/java/com/example/mvvmproject/viewmodel/LoginViewModel.kt
+++ mvvmproject/src/main/java/com/example/mvvmproject/viewmodel/LoginViewModel.kt
@@ -1,5 +1,42 @@
 package com.example.mvvmproject.viewmodel
 
-class LoginViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.mvvmproject.base.BaseViewModel
+import com.example.mvvmproject.netwrok.DTO.Login
+import com.example.mvvmproject.netwrok.DTO.Responselogin
+import com.example.mvvmproject.netwrok.api.UserAPI
+import com.example.mvvmproject.widget.SingleLiveEvent
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
+class LoginViewModel (private val service : UserAPI) : BaseViewModel() {
+
+    val onSuccessEvent = SingleLiveEvent<Unit>()
+    val onFailEvent = SingleLiveEvent<Unit>()
+    val onErrorEvent = SingleLiveEvent<Unit>()
+    val onSignUpEvent = SingleLiveEvent<Unit>()
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
+    fun checkLogin(){
+        Log.d("TEST", "T")
+        if (email.value != null && password.value != null) {
+            if (email.value!!.isNotEmpty() && password.value!!.isNotEmpty()) {
+                service.login(Login( email.value.toString(),password.value.toString())).enqueue(object : retrofit2.Callback<Responselogin>{
+                    override fun onResponse(
+                        call: Call<Responselogin>,
+                        response: Response<Responselogin>
+                    ) {
+
+                    }
+
+                    override fun onFailure(call: Call<Responselogin>, t: Throwable) {
+                    }
+
+                })
+            }
+        }
+    }
 }
