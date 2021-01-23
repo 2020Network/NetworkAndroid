785c53e27e6eb7fb4214199ce910b344efdaccbf
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2021-01-23 23:07:46 +0900

    <Update> di Update Login

--- mvvmproject/src/main/java/com/example/mvvmproject/di/appModule.kt
+++ mvvmproject/src/main/java/com/example/mvvmproject/di/appModule.kt
@@ -1,10 +1,24 @@
 package com.example.mvvmproject.di
 
+import com.example.mvvmproject.netwrok.api.UserAPI
+import com.example.mvvmproject.viewmodel.LoginViewModel
 import com.example.mvvmproject.viewmodel.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
 
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://189893f93a94.ngrok.io")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    single {
+        retrofit.create(UserAPI::class.java)
+    }
+
     viewModel { SplashViewModel() }
+    viewModel { LoginViewModel(get<UserAPI>()) }
 }
