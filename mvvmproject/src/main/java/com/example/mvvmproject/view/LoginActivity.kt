67911caa55aebeef8555f35001b6942669928146
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2021-01-23 23:08:17 +0900

    <Update> view Login SingUp

--- mvvmproject/src/main/java/com/example/mvvmproject/view/LoginActivity.kt
+++ mvvmproject/src/main/java/com/example/mvvmproject/view/LoginActivity.kt
@@ -1,12 +1,48 @@
 package com.example.mvvmproject.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.mvvmproject.R
+import com.example.mvvmproject.base.BaseActivity
+import com.example.mvvmproject.databinding.ActivityLoginBinding
+import com.example.mvvmproject.viewmodel.LoginViewModel
+import com.example.mvvmproject.widget.extension.longToastMessage
+import com.example.mvvmproject.widget.extension.shortToastMessage
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+class LoginActivity :BaseActivity<LoginViewModel, ActivityLoginBinding>() {
+    override val resource: Int
+        get() = R.layout.activity_login
+    override val viewModel: LoginViewModel
+        get() = getViewModel(LoginViewModel::class)
+
+    override fun init() {
     }
+
+    override fun observerViewModel() {
+        with(viewModel){
+            onSuccessEvent.observe(this@LoginActivity, {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
+            onFailEvent.observe(this@LoginActivity, {
+
+                shortToastMessage("로그인을 실패하였습니다.")
+            })
+            onErrorEvent.observe(this@LoginActivity, {
+                shortToastMessage("로그인 에러")
+            })
+
+            onSignUpEvent.observe(this@LoginActivity, {
+                val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+                startActivity(intent)
+                finish()
+            })
+        }
+    }
+
 }
