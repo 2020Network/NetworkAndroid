c521c963224ca44e05676362962b7e4e9f958d39
kjw12qwas <49026547+kjw12qwas@users.noreply.github.com>
2021-01-26 22:53:17 +0900

    <Update> Signup ViewModel

--- mvvmproject/src/main/java/com/example/mvvmproject/viewmodel/SignUpViewModel.kt
+++ mvvmproject/src/main/java/com/example/mvvmproject/viewmodel/SignUpViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.mvvmproject.viewmodel
 
-class SignUpViewModel {
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
+import androidx.lifecycle.MutableLiveData
+import com.example.mvvmproject.base.BaseViewModel
+import com.example.mvvmproject.netwrok.api.UserAPI
+import com.example.mvvmproject.widget.SingleLiveEvent
+
+class SignUpViewModel(private val service : UserAPI) : BaseViewModel() {
+    val onSuccessEvent = SingleLiveEvent<Unit>()
+    val onFailEvent = SingleLiveEvent<Unit>()
+    val onErrorEvent = SingleLiveEvent<Unit>()
+    val onSignUpEvent = SingleLiveEvent<Unit>()
+
+    val name = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val password2 = MutableLiveData<String>()
+    val age = MutableLiveData<String>()
+    val gender = MutableLiveData<RadioGroup>()
+    val clicksListener = object : AdapterView.OnItemSelectedListener {
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+        }
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            parent?.getItemAtPosition(position) as String
+        }
+    }
+
+    fun checkSign(){
+        Log.d("TEST", "T")
+
+    }
 }
